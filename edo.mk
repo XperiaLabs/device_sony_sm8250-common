4fc92bbccb1ae1f9b64ec9dd0c26dfbe9799fb9a
Demon Singur <demonsingur@gmail.com>
2018-07-14 14:37:51 +0000

    sm8250-common: do not build useless packages
    They are compiled as dependencies of ipacm
    
    Change-Id: I72076113083308e26573289923f5d3c00a8a6b44

--- edo.mk
+++ edo.mk
@@ -463,7 +463,6 @@ PRODUCT_PACKAGES += \
     libprotobuf-cpp-full-3.9.1-vendorcompat \
     libprotobuf-cpp-lite-3.9.1-vendorcompat \
     librmnetctl \
-    libxml2 \
     extphonelib \
     extphonelib.xml
 
