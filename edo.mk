80af7f2a4ce6b0a9fdb4a11789db4feb6b03aad1
George Chang <georgekgchang@google.com>
2022-07-21 14:18:52 +0800

    edo: Add sku info for enable nfc secure
    Bug: 239668161
    Test: check settings page
    Change-Id: I43d4c4f4f29c6746cdaff90361c04b6720f5ff3e

--- edo.mk
+++ edo.mk
@@ -43,6 +43,7 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     SonyEdoFrameworksResCommon \
+    SonyEdoNfcOverlay \
     SonyEdoSettingsResCommon \
     SonyEdoSystemUIResCommon \
     SonyEdoTelephonyResCommon \
