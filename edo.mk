79e463f0bb721a2f7a6ac93599ca8b0709e12534
Michael Bestas <mkbestas@gmail.com>
2022-11-07 17:49:42 +0200

    sm8250-common: disable autobrightness by default

--- edo.mk
+++ edo.mk
@@ -46,7 +46,6 @@ PRODUCT_ENFORCE_RRO_TARGETS := *
 PRODUCT_PACKAGES += \
     CarrierConfigResCommon \
     SonyEdoFrameworksResCommon \
-    SonyEdoSettingsProviderOverlayCommon \
     SonyEdoSettingsResCommon \
     SonyEdoSystemUIResCommon \
     SonyEdoTelephonyResCommon \
