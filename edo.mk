cf4f8c6fced597db534c9221d1cc55d7ec825095
Han Sol Jin <hansol@hansol.ca>
2024-04-26 19:41:02 -0700

    edo: build common Lineage power-libperfmgr HAL
    * powerhint.json taken from https://github.com/EmanuelCN/android_device_xiaomi_sm8250-common/commits/1e96a6095d953581e15181cb45aa908abc646617/configs/powerhint.json
    
    Change-Id: I141ba31cff3a8bf6d541ac81cbd570f8b1a8a507

--- edo.mk
+++ edo.mk
@@ -404,6 +404,18 @@ PRODUCT_PACKAGES += \
     libOmxVenc \
     libstagefrighthw
 
+# Power
+PRODUCT_PACKAGES += \
+    android.hardware.power-service.lineage-libperfmgr
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/google/interfaces \
+    hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr
+
 # QMI
 PRODUCT_PACKAGES += \
     libcurl.vendor \
